4e7f707be00abe51192cbc0418785ccbdc8cb5ea
Samuel <samuel.santschi@gmail.com>
2023-06-20 10:12:45 +0200

    Add input and output dir as application properties

--- src/main/java/fr/manta2i/photoorganizer/PhotoOrganizerApplication.java
+++ src/main/java/fr/manta2i/photoorganizer/PhotoOrganizerApplication.java
@@ -7,18 +7,37 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 public class PhotoOrganizerApplication implements CommandLineRunner {
 
-    private static final Logger LOG = LoggerFactory.getLogger(PhotoOrganizerApplication.class);
+    private static final Logger LOG = LoggerFactory
+            .getLogger(PhotoOrganizerApplication.class);
 
-    private final List<String> inputDirectory = List.of("C:\\Users\\Samuel\\Pictures\\onePlus", "U:\\Images\\Pixel");
+
+    private final List<String> inputDirectory;
+
+    private final File outputDirectory;
 
     private final OrderingFileService orderingFileService;
 
-    public PhotoOrganizerApplication(OrderingFileService orderingFileService) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MM")
+            .withZone(ZoneId.systemDefault());
+
+    public PhotoOrganizerApplication(@Value("${inputDirectory}") List<String> inputDirectory,
+                                     @Value("${outputDirectory}") File outputDirectory,
+                                     OrderingFileService orderingFileService) {
+        this.inputDirectory = inputDirectory;
+        this.outputDirectory = outputDirectory;
         this.orderingFileService = orderingFileService;
     }
 
@@ -31,7 +50,7 @@ public class PhotoOrganizerApplication implements CommandLineRunner {
         LOG.info("EXECUTING : command line runner");
 
         inputDirectory.stream()
-                .peek(s -> LOG.info("Starting input {}", s))
+                .peek(s -> LOG.info("Starting input {}" ,s ))
                 .map(File::new)
                 .filter(File::exists)
                 .forEach(orderingFileService::orderDirectory)
